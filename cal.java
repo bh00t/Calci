b44221e6b78eae4433a8a558b3a50506947dc8e1
RISHABH TRIPATHI <rishabh4u@hotmail.com>
2015-08-07 07:00:21 -0700

    all listener are added
    limit of calculation need to increase from double to BigDecimal

--- cal.java
+++ cal.java
@@ -155,14 +155,88 @@ class click implements ActionListener,MouseListener,KeyListener
 	public void mouseClicked(MouseEvent e){}
 //////////////////////////////////////////////////////////////////////////////
 
-	public void keyTyped(KeyEvent e) {
-	 	if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE)
-    {  
-        System.out.println(e.getKeyChar() + "typed VK_BACK_SPACE" +e.getKeyCode());
+	public void keyTyped(KeyEvent e) 
+	{
+	 	int key = (int)e.getKeyChar();
+
+	 	if(key >= 48 && key<=57)
+	 	{
+	 		s=c1.input(Character.toString((char)key));
+	 		
+	 	}
+	 	else
+		 	switch(key)
+		 	{
+		 		case 97:
+		 		case 65:
+		 		case 43:
+		 				s=c1.input("+");
+		 			//	display.input.setText("+");
+		 				break;
+		 		case 115:
+		 		case 83:
+		 		case 45:
+		 				s=c1.input("-");
+		 			//	display.input.setText("-");
+		 				break;
+		 		case 109:
+		 		case 77:
+		 		case 42:
+		 				s=c1.input("*");
+		 			//	display.input.setText("*");
+		 				break;
+		 		case 100:
+		 		case 68:
+		 		case 47:
+		 				s=c1.input("/");
+		 			//	display.input.setText("/");
+		 				break;
+		 		case 112:
+		 		case 37:
+		 		case 80:
+		 				s=c1.input("%");
+		 			//	display.input.setText("%");
+		 				break;
+		 		case 67:
+		 		case 99:
+		 				s=c1.input("C");
+		 			//	display.input.setText("C");
+		 				break;
+		 		case 127:
+		 				s=c1.input("CE");
+		 				//display.input.setText("CE");
+		 				break;
+		 		case 8:
+		 				s=c1.input("D");
+		 				//display.input.setText("D");
+		 				break;
+		 		case 46:
+		 				s=c1.input(".");
+		 				//display.input.setText(".");
+		 				break;
+		 		case 61:
+		 		case 10:
+		 				s=c1.input("=");
+		 				display.input.setText("=");
+		 				break;
+		 		case 105:
+		 		case 73:
+		 				s=c1.input("\u00B1");
+		 				break;
+		 		case 114:
+		 		case 82:
+		 				s=c1.input("1/x");
+		 				break;
+		 		case 117:
+		 		case 85:
+		 				s=c1.input("\u221A");
+		 				break;
+		 		default : break;
+
+		 	}
+		 	display.input.setText(s);
+		 	
     }
-    else
-	 	System.out.println((int)e.getKeyChar() + "typed " +e.getKeyCode());
-	 }
 
       public void keyPressed(KeyEvent e){}
 
@@ -472,7 +546,10 @@ class function
 		{
 			Toolkit.getDefaultToolkit().beep();
 			calculation.stop=1;
-			return "Cannot divide by zero";
+			if(y!=0)
+				return Double.toString(z);
+			else
+				return "Cannot divide by zero";
 		}
 		else if(Double.toString(z).compareTo("NaN")==0)
 		{
@@ -503,7 +580,7 @@ class function
 			default:break;
 		}
 
-		if(Double.toString(z)=="Infinity")
+		if(x==0)
 		{
 			Toolkit.getDefaultToolkit().beep();
 			calculation.stop=1;
